5354fad211831f04debd6b50efe8383e35624896
Kevin Riedlberger <34638@novasbe.pt>
2020-07-20 00:02:13 +0200

    Fixed syntax inconsistency

--- String Manipulation/Sherlock and the Valid String/sherlockstring_solution.py
+++ String Manipulation/Sherlock and the Valid String/sherlockstring_solution.py
@@ -11,7 +11,7 @@ def isValid(s):
     # If every character appear the exact same amount of times, the length of
     # frequency is equal to one and we can return 'YES'
     if len(freq) == 1:
-        return "YES"
+        return 'YES'
 
     # Otherwise the length of frquency can only be 2, since more than 2
     # different frequencies would result in an unvalid string
@@ -24,15 +24,15 @@ def isValid(s):
         # frequency is also just 1, the string can be adhusted by removing one
         # character and is therefore ruled as valid
         if key_max - key_min == 1 and freq[key_max] == 1:
-            return "YES"
+            return 'YES'
         # Another case for a valid string would be if the appearances of the
         # minimum key is only one and we can drop this character to generate an
         # equal amount of appearances for each character
         elif key_min == 1 and freq[key_min] == 1:
-            return "YES"
+            return 'YES'
 
     # If no check is passed return 'NO'
-    return "NO"
+    return 'NO'
 
 
 # Test
